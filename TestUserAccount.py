12e090b98c828151e47822268c60241d51ddb508
sivan18-meet <sbmislovely@gmail.com>
2016-11-24 19:56:20 +0200

    yay

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,13 +1,13 @@
 #This script performs some simple tests on the UserAccount class.
-import UserAccount
+from UserAccount import *
 
 #Two things are missing from the line below - fill them in
-my_user=UserAcount(  ,  )
+my_user=UserAccount("user" ,"123" ,"secret" )
 
 #Call the print_password method (function) - it takes one input - a guess for the password.
-
+my_user.print_password("12345")
 #Use the wrong password as input here
-my_user.
+my_user.password=12345
 
 #Use the right password here
-my_user.
+my_user.password=123
